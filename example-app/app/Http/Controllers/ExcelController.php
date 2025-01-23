d3d7a5d6448b6599d4c0af3c67f0d502956bcfd3
hajarLafdaoui <155734398+hajarLafdaoui@users.noreply.github.com>
2025-01-23 11:00:43 +0100

    Styling the UploadFile component, Displaying only the cells with notes and skip rendering empty cells, Extract the computed value of the formula

--- example-app/app/Http/Controllers/ExcelController.php
+++ example-app/app/Http/Controllers/ExcelController.php
@@ -1,18 +1,11 @@
 <?php
 
-
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use PhpOffice\PhpSpreadsheet\IOFactory;
-
 class ExcelController extends Controller
 {
-    public function showUploadForm()
-    {
-        return view('upload');
-    }
+
 
     public function import(Request $request)
     {
@@ -30,11 +23,19 @@ class ExcelController extends Controller
             $cellIterator->setIterateOnlyExistingCells(false);
             $rowData = [];
             foreach ($cellIterator as $cell) {
-                $rowData[] = $cell->getValue();
+                $value = $cell->getCalculatedValue();
+                
+                // Check if the value is numeric and format it to 2 decimal places
+                if (is_numeric($value)) {
+                    $value = number_format($value, 2, '.', '');
+                }
+    
+                $rowData[] = $value;
             }
             $notes[] = $rowData;
         }
 
         return response()->json($notes);
     }
+
 }
